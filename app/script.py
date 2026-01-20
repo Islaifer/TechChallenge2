93bb7cec536e51c687731fd93d1560844d2dc037
Adryen Simoes <islaifer2379@gmail.com>
2026-01-20 02:05:20 -0300

    Finish scraping

--- app/script.py
+++ app/script.py
@@ -1,54 +1,67 @@
 import requests
 import base64
-import boto3
+import os
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from io import BytesIO
 from datetime import date
 
 
-s3 = boto3.client('s3')
 
-BUCKET_NAME = 'techchallenge-bovespa'
-S3_PREFIX = 'raw/b3'
+BASE_PATH = os.path.expanduser(
+    "~/Documentos/Study/TechChalenges/Tech2Parquets"
+)
+LOCAL_PREFIX = "raw/b3" 
 
-def convert_to_base64(text: str):
+
+def convert_to_base64(text: str) -> str:
     texto_bytes = text.encode("utf-8")
     base64_bytes = base64.b64encode(texto_bytes)
-    base64_str = base64_bytes.decode("utf-8")
-    
-    return base64_str
+    return base64_bytes.decode("utf-8")
 
 
-def lambda_handler(event, context):
-    base_url = 'https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetPortfolioDay/'
+def extract_ibov_data():
+    base_url = (
+        "https://sistemaswebb3-listados.b3.com.br/"
+        "indexProxy/indexCall/GetPortfolioDay/"
+    )
+
     page_number = 1
     extraction_date = date.today().isoformat()
     records = []
+
     while True:
-        page_search = f'{{"language":"pt-br","pageNumber":{page_number},"pageSize":20,"index":"IBOV","segment":"1"}}'
-        page_search_encoded = convert_to_base64(page_search)
-        url = base_url + page_search_encoded
-        response = requests.get(url)
-    
-        if(response.status_code != 200):
+        page_search = (
+            f'{{"language":"pt-br","pageNumber":{page_number},'
+            f'"pageSize":20,"index":"IBOV","segment":"1"}}'
+        )
+
+        url = base_url + convert_to_base64(page_search)
+        response = requests.get(url, timeout=30)
+
+        if response.status_code != 200:
             raise Exception(f"Erro na API B3: {response.text}")
-    
+
         data = response.json()
-        assets = data['results']
-    
+        assets = data.get("results", [])
+
         for asset in assets:
-            asset['extraction_date'] = extraction_date
+            asset["extraction_date"] = extraction_date
             records.append(asset)
-        
-        total_pages = data['page']['totalPages']
-        if(page_number >= total_pages):
+
+        total_pages = data["page"]["totalPages"]
+        if page_number >= total_pages:
             break
-        
+
         page_number += 1
-    
-    df = pd.DataFrame(records)
+
+    return pd.DataFrame(records), extraction_date
+
+
+'''
+Método para mandar ao s3
+def upload_to_s3(df: pd.DataFrame, extraction_date: str):
+    s3 = boto3.client("s3")
 
     buffer = BytesIO()
     table = pa.Table.from_pandas(df)
@@ -63,8 +76,40 @@ def lambda_handler(event, context):
         Body=buffer.getvalue()
     )
 
-    return {
-        "statusCode": 200,
-        "message": f"Arquivo salvo em s3://{BUCKET_NAME}/{s3_key}",
-        "records": len(df)
-    }
+    return s3_key
+'''
+
+def save_local(df: pd.DataFrame, extraction_date: str):
+    dir_path = os.path.join(
+        BASE_PATH,
+        LOCAL_PREFIX,
+        f"dt={extraction_date}"
+    )
+
+    os.makedirs(dir_path, exist_ok=True)
+
+    file_path = os.path.join(dir_path, "b3_ibov.parquet")
+
+    print("DIR PATH:", dir_path)
+    print("DIR EXISTS:", os.path.exists(dir_path))
+    print("FILE PATH:", file_path)
+
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, file_path)
+
+    return file_path
+
+def main():
+    print("Iniciando extração dos dados do IBOV...")
+    df, extraction_date = extract_ibov_data()
+
+    #print(f"{len(df)} registros extraídos. Enviando para o S3...")
+    #s3_key = upload_to_s3(df, extraction_date)
+
+    file_location = save_local(df, extraction_date)
+    print("Arquivo salvo com sucesso em:")
+    print(f"{file_location}")
+
+
+if __name__ == "__main__":
+    main()
